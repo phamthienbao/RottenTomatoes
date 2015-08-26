a3799f1eaf05a481964c47ad0ebdbc51ed4c43f5
Bao Pham <phamthienbao@Baos-MacBook-Pro.local>
2015-08-26 20:00:32 +0700

    Initial Commit

---
+++ RottenTomatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RottenTomatoes
+//
+//  Created by Bao Pham on 8/26/15.
+//  Copyright (c) 2015 Bao Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
