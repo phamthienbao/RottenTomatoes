5f2b42e856704a8924258247a893fac113d8ba1e
Bao Pham <phamthienbao@baos-macbook-pro.local>
2015-09-01 00:46:03 +0700

    add gift file

---
+++ RottenTomatoes/MoviesCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesCell.swift
+//  RottenTomatoes
+//
+//  Created by Bao Pham on 8/30/15.
+//  Copyright © 2015 Bao Pham. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RottenTomatoes/NetworkChecking.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkChecking.swift
+//  RottenTomatoes
+//
+//  Created by Bao Pham on 8/31/15.
+//  Copyright © 2015 Bao Pham. All rights reserved.
+//
+
+import Foundation
